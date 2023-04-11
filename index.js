949b389abe457f0d67c4b3ade6c44e93a498c03a
Pablo Gonzalez <ezekielgonzalezr@gmail.com>
2023-04-11 07:32:02 -0600

    Interrupt scroll

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const burger = document.querySelector('.menu');
 const opts = document.querySelector('.option');
 const close = document.getElementsByClassName('close');
+const body = document.querySelector('.body');
 
 burger.onclick = () => {
   opts.classList.add('display');
+  body.classList.add('overflow');
 };
 
 for (let i = 0; i < close.length; i += 1) {
   close[i].onclick = () => {
     opts.classList.remove('display');
+    body.classList.remove('overflow');
   };
 }
