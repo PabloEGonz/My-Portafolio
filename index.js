4af3038fabbf41918ebe6d79bfa42d4cb16384b9
Pablo Gonzalez <ezekielgonzalezr@gmail.com>
2023-04-10 21:20:36 -0600

    Close button added on js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const burger = document.querySelector('.menu');
 const opts = document.querySelector('.option');
 const close = document.querySelector('.close');
 
+
 burger.onclick =() => {
-    console.log(burger)
     opts.classList.add('display')
 };
+
+close.onclick = () => {
+ opts.classList.remove('display');
+}
