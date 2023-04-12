54001199db77f3e64ee3eafe411761abd1df2e25
Pablo Gonzalez <ezekielgonzalezr@gmail.com>
2023-04-11 22:17:19 -0600

    Create array with objects

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
+const projects = [ project1 = {
+  name : 'project 1',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Css', 'Bootstrap', 'Ruby'],
+  live : '',
+  repo : ''
+},
+project2 = {
+  name : 'project 2',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Css', 'Bootstrap'],
+  live : '',
+  repo : ''
+},
+project3 = {
+  name : 'project 3',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Bootstrap', 'Ruby'],
+  live : '',
+  repo : ''
+},
+project4 = {
+  name : 'project 4',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Bootstrap', 'Ruby'],
+  live : '',
+  repo : ''
+},
+project5 = {
+  name : 'project 5',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Bootstrap', 'Ruby'],
+  live : '',
+  repo : ''
+},
+project6 = {
+  name : 'project 6',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Css', 'Bootstrap', 'Ruby'],
+  live : '',
+  repo : ''
+},
+project7 = {
+  name : 'project 7',
+  description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In tellus integer feugiat scelerisque. Sed felis eget velit aliquet sagittis id consectetur purus. Sodales neque sodales ut etiam sit.',
+  image : '',
+  techs : ['HTML', 'Bootstrap', 'Ruby'],
+  live : '',
+  repo : ''
+}
+]
+console.log(projects[0]);
+
+
+
 const burger = document.querySelector('.menu');
 const opts = document.querySelector('.option');
 const close = document.getElementsByClassName('close');
